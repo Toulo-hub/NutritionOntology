7b6eab76516ec3628adcc7aa55b84f424f422400
Toulo-hub <pcgroupe1@gmail.com>
2021-07-05 18:05:28 +0200

    premier commit du frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ResultatComponent } from './resultat/resultat.component';
+import { AlimentsService } from './aliments.service';
+import { AjouterComponent } from './ajouter/ajouter.component';
+import { RequeteComponent } from './requete/requete.component';
+import { ValiderComponent } from './valider/valider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ResultatComponent,
+    AjouterComponent,
+    RequeteComponent,
+    ValiderComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AlimentsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
